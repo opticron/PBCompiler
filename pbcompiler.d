edfed9a43b46282f65dd7fccf3b615200dfef8e4
Kinsey Moore <nyphbl8d@gmail.com>
2009-07-20 18:34:31 +0000

    support imports!

--- pbcompiler.d
+++ pbcompiler.d
@@ -8,20 +8,31 @@ int main(char[][]args) {
 	args = args[1..$];
 	if (!args.length) throw new Exception("No proto files supplied on the command line!");
 	foreach (arg;args) {
-		char[]contents = cast(char[])read(arg);
-		auto root = PBRoot(contents);
-		char[]fname = root.Package;
-		if (!fname.length) {
-			if (arg.length>6 && arg[$-6..$].icmp(".proto") == 0) {
-				fname = arg[0..$-6]~".d";
-				// we want to grab the name here
-			} else {
-				fname = arg~".d";
-			}
-		} else {
-			fname ~= ".d";
-		}
-		write(fname,root.toDString);
+		compileRoot(arg);
 	}
 	return 0;
+}
+
+// returns package name
+char[]compileRoot(char[]filename) {
+	char[]contents = cast(char[])read(filename);
+	auto root = PBRoot(contents);
+	char[]fname = root.Package;
+	if (!fname.length) {
+		if (filename.length>6 && filename[$-6..$].icmp(".proto") == 0) {
+			fname = filename[0..$-6]~".d";
+			// we want to grab the name here
+		} else {
+			fname = filename~".d";
+		}
+	}
+	char[]tmp;
+	tmp ~= "module "~fname~";\n";
+	foreach(imp;root.imports) {
+		tmp ~= "import "~compileRoot(imp)~";\n";
+	}
+	tmp ~= root.toDString;
+	fname ~= ".d";
+	write(fname,tmp);
+	return fname[0..$-2];
 }
